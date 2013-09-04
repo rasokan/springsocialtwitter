3a271284548099e983a78a47ba9566b424f88f11
rasokan <rashok55@gmail.com>
2013-09-04 13:41:37 +0530

    Remove target

--- target/classes/com/learn/spring/social/twitter/repository/UsernameAlreadyInUseException.java
+++
@@ -1,9 +0,0 @@
-package com.learn.spring.social.twitter.repository;
-
-public class UsernameAlreadyInUseException extends Exception {
-	
-	public UsernameAlreadyInUseException(String username) {
-		super("The username '" + username + "' is already in use.");
-	}
-
-}
